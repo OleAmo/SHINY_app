e940000e08ca4bd00f6164ce3184dc6dbe346594
Oleguer <oleguer.amoros@gmail.com>
2026-04-03 18:14:59 +0200

    commit

--- app_v2.R
+++ app_v2.R
@@ -34,8 +34,8 @@ server <- function(input, output) {
 shinyApp(ui, server)
 
 
-#  --------- EXEMPLE 02 ---------
-#  ------------------------------
+#  --------- EXEMPLE 02 - TEXT, TAULA --------
+#  -------------------------------------------
 
 #    -) Creo DOS SLIDERS INPUT = Temp Max i Temp Min
 #    -) Els REPRESENTO de 3 FORMES
@@ -93,14 +93,110 @@ server <- function(input, output) {
 shinyApp(ui, server)
 
 
+#  --------- EXEMPLE 02 - HTML --------
+#  -------------------------------------------
+
+#    -) Creo DOS SLIDERS INPUT = Temp Max i Temp Min
+#    -) Ho faig en HTML
 
 
+ui <- fluidPage(
+  sliderInput(
+    "temp_max",
+    label = "Temperatura Max",
+    min = 17, 
+    max = 31, 
+    value = 15
+  ),
+  sliderInput(
+    "temp_min",
+    label = "Temperatura Min",
+    min = -5, 
+    max = 18, 
+    value = 9
+  ),
+  htmlOutput("resumHTML")
+)
+
+server <- function(input, output) {
+  
+  output$resumHTML <- renderUI({
+    
+    tmax <- input$temp_max
+    tmin <- input$temp_min
+    
+    HTML(paste0(
+      "<h3 style='color:blue;'>EXEMPLE TAULA HTML</h2>",
+      
+      "<h4><b>Les TEMPERATURES:<b></h4>",
+      "<ul>",
+      "<li>La Temperatura màxima = ",tmax," ºC</li>",
+      "<li>La Temperatura mínim = ",tmin,"ºC</li>",
+      "</ul>"
+      
+    ))
+    
+  })
+}
+
+server_v2 <- function(input, output) {
+  
+  output$resumHTML <- renderUI({
+    
+    tmax <- input$temp_max
+    tmin <- input$temp_min
+    
+    HTML(glue::glue("
+    
+      <h3 
+         style='
+         font-family: arial, sans-serif;
+         background-color: #A6C0FF;
+         color: #223B7D;
+         width: 150px
+         '
+      >HTML Table</h3>
+      
+      <div 
+         style='
+         font-family: arial, 
+         sans-serif;
+         width: 150px;
+         border: 2px solid #FF1C1C;
+         '
+        >
+       <table
+          style='
+          width: 150px;
+       '>
+        <tr style='background-color: #99FF94;'>
+          <th>T Màx</th>
+          <th>T Mín</th>
+        </tr>
+        <tr>
+          <td>{tmax}</td>
+          <td>{tmin}</td>
+        </tr>
+        <tr>
+          <td>{tmax*2}</td>
+          <td>{tmin*2}</td>
+        </tr>
+        <tr>
+          <td>{tmax*3}</td>
+          <td>{tmin*3}</td>
+        </tr>
+        
+      </table>
+      </div>
+
+    "))
+    
+  })
+}
 
 
-
-
-
-
+shinyApp(ui, server)
+shinyApp(ui, server_v2)
 
 
 
