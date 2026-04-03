d8986d5e4efa5145d5fa3342367fab3560565dad
Oleguer <oleguer.amoros@gmail.com>
2026-04-03 17:32:45 +0200

    commit

--- app_v2.R
+++ app_v2.R
@@ -8,16 +8,89 @@ library(shiny)
 #  ------------------------------
 
 #    -) Exemple de la web de SHINY R
-#    -) UI
-#    -) SERVER
-#    -) APPY SHINY
+#    -) Num de alumnes
+#    -) Mitjana de NOTES
 
 
+ui <- fluidPage(
+  sliderInput(
+    "n",
+    label = "Nombre d'alumnes:",
+    min = 10, 
+    max = 100, 
+    value = 30
+  ),
+  textOutput("mitjanaText")
+)
+
+server <- function(input, output) {
+  
+  output$mitjanaText <- renderText({
+    notes <- rnorm(input$n, mean = 6, sd = 1)
+    paste("La mitjana de les notes és:", round(mean(notes), 2))
+  })
+}
+
+shinyApp(ui, server)
 
 
+#  --------- EXEMPLE 02 ---------
+#  ------------------------------
+
+#    -) Creo DOS SLIDERS INPUT = Temp Max i Temp Min
+#    -) Els REPRESENTO de 3 FORMES
+#    -) TEXT, TAULA i TAULA PRO
 
 
+ui <- fluidPage(
+  sliderInput(
+    "temp_max",
+    label = "Temperatura Max",
+    min = 17, 
+    max = 31, 
+    value = 15
+  ),
+  sliderInput(
+    "temp_min",
+    label = "Temperatura Min",
+    min = -5, 
+    max = 18, 
+    value = 9
+  ),
+  textOutput("temp_text"),
+  tableOutput("temp_taula"),
+  dataTableOutput("temp_taula_Pro")
+)
 
+server <- function(input, output) {
+  
+  output$temp_text <- renderText({
+    tmax <- input$temp_max
+    tmin <- input$temp_min
+    
+    mitja <- round(tmax)
+    
+    paste0("Temp màxima =",tmax,"  Temp mínima =",tmin)
+  })
+  
+  output$temp_taula <- renderTable({
+    tmax <- input$temp_max
+    tmin <- input$temp_min
+    
+    data.frame(Temp_max = tmax, Temp_min = tmin)
+  })
+  
+  output$temp_taula_Pro <- renderDataTable({
+    tmax <- input$temp_max
+    tmin <- input$temp_min
+    
+    data.frame(Temp_max = tmax, Temp_min = tmin)
+  })
+  
+  
+}
+
+shinyApp(ui, server)
 
 
 
